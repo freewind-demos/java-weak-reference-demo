a8883b8512c6f64e2056f6f5bb3385f5e406a6c5
freewind <nowindlee@gmail.com>
2018-07-25 18:11:11 +0800

    weak demo done

--- README.md
+++ README.md
@@ -1,4 +1,47 @@
-Java Hello World Demo
-=====================
+Java Weak Reference Demo
+========================
 
-Run `Hello.java` in your IDE.
+Understand weak reference.
+
+- weak reference: When Jvm runs `gc`, if there is any weak reference, they will be released, even if the heap memory is not full.
+- soft reference: Soft references are only released when heap is full when GC
+
+Run
+---
+
+Run `Hello.java` in your IDE, and set the vm options to:
+
+- `-XX:+PrintGC`: print gc information in console
+
+Since we create very small amount of objects in this demo, we don't need to specify the heap size like we do in other demos.
+
+Log in console
+--------------
+
+The output may like:
+
+```
+cache size: 10
+[GC (System.gc())  5252K->824K(251392K), 0.0013236 secs]
+[Full GC (System.gc())  824K->653K(251392K), 0.0066789 secs]
+# Released weak references: 0
+cache size: 20
+[GC (System.gc())  3274K->789K(251392K), 0.0006443 secs]
+[Full GC (System.gc())  789K->615K(251392K), 0.0071458 secs]
+# Released weak references: 10
+cache size: 20
+[GC (System.gc())  3237K->743K(251392K), 0.0003727 secs]
+[Full GC (System.gc())  743K->615K(251392K), 0.0064726 secs]
+# Released weak references: 10
+cache size: 20
+[GC (System.gc())  1925K->743K(251392K), 0.0003508 secs]
+[Full GC (System.gc())  743K->615K(251392K), 0.0024689 secs]
+# Released weak references: 10
+cache size: 20
+[GC (System.gc())  1925K->711K(251392K), 0.0004487 secs]
+[Full GC (System.gc())  711K->615K(251392K), 0.0023783 secs]
+# Released weak references: 10
+```
+
+You can see the `Released weak references` is great then 0 for each `GC` after the first time, even if there is still a very large amount of Heap size.
+
--- src/main/java/demo/Hello.java
+++ src/main/java/demo/Hello.java
@@ -1,9 +1,77 @@
 package demo;
 
+import java.lang.ref.Reference;
+import java.lang.ref.ReferenceQueue;
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 public class Hello {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    private static ReferenceQueue<User> releaseLog = new ReferenceQueue<>();
+
+    private static ArrayList<WeakReference<User>> cache = new ArrayList<WeakReference<User>>() {
+        @Override
+        public boolean add(WeakReference<User> userWeakReference) {
+            removeReleasedItems();
+            return super.add(userWeakReference);
+        }
+        private void removeReleasedItems() {
+            for (int i = this.size() - 1; i >= 0; i--) {
+                WeakReference<User> item = this.get(i);
+                if (item.get() == null) {
+                    this.remove(i);
+                }
+            }
+        }
+    };
+
+    public static void main(String[] args) throws InterruptedException {
+        while (true) {
+            List<User> users = generateUsers(10);
+            for (User user : users) {
+                WeakReference<User> reference = new WeakReference<>(user, releaseLog);
+                cache.add(reference);
+            }
+
+            System.out.println("cache size: " + cache.size());
+
+            System.gc();
+
+            printReleasedUsers(releaseLog);
+
+            Thread.sleep(1000);
+        }
     }
 
+    private static void printReleasedUsers(ReferenceQueue<User> queue) throws InterruptedException {
+        int timeout = 10;
+        int releaseCount = 0;
+        while (true) {
+            Reference<? extends User> ref = queue.remove(timeout);
+            if (ref == null) {
+                break;
+            }
+            releaseCount += 1;
+        }
+        System.out.println("# Released weak references: " + releaseCount);
+    }
+
+    private static List<User> generateUsers(int count) {
+        ArrayList<User> users = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            users.add(new User("" + i));
+        }
+        return users;
+    }
+
+}
+
+class User {
+    final String id = UUID.randomUUID().toString();
+    final String name;
+    public User(String name) {
+        this.name = name;
+    }
 }
